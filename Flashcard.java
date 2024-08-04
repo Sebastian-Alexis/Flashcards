5d4c1456b1fcaac494e86e23a5a826757b561c5e
SebastianAlexis <sebastianralexis@gmail.com>
2024-08-04 16:00:08 -0700

    Implemented basic flashcard functionality

---
+++ Flashcard.java
@@ -0,0 +1,31 @@
+public class Flashcard {
+    private int id;
+    private String question;
+    private String answer;
+
+    public Flashcard(int id, String question, String answer) {
+        this.id = id;
+        this.question = question;
+        this.answer = answer;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+}
